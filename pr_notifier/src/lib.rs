82865d6fd70aec5cf37ad8963305907772b8a197
Yu Zhu <bcct2010@live.cn>
2022-07-31 23:00:52 +0800

    pr_notifier: initial commits (#2)

--- pr_notifier/src/lib.rs
+++ pr_notifier/src/lib.rs
@@ -7,9 +7,32 @@ mod tests {
     }
 }
 
+use gjson;
 #[allow(unused_imports)]
 use wasmedge_bindgen::*;
+
 #[wasmedge_bindgen_macro::wasmedge_bindgen]
-pub fn run(msg :String) -> Result<String,String> {
-    return Ok(msg);
+pub fn run(event :String) -> Result<String,String> {
+    let mut final_msg = String::from("Github PR: ");
+    
+    let owned_event = event.as_str().to_owned();
+    let action = gjson::get(&owned_event, "action");
+    let usr_name = gjson::get(&owned_event, "pull_request.user.login");
+
+    let mut tmp_msg = String::from("User named ");
+    tmp_msg.push_str(usr_name.str());
+    tmp_msg.push_str(" ");
+    tmp_msg.push_str(action.str());
+    tmp_msg.push_str(" a pull request");
+
+    if action.str() == "opened" || action.str() == "reopened" {
+        let is_draft = gjson::get(&owned_event, "pull_request.draft");
+        if is_draft.bool() {
+            tmp_msg.push_str(" as a draft");  
+        }
+    }
+
+    final_msg.push_str(tmp_msg.as_str());
+    final_msg.push_str("!");
+    return Ok(final_msg);
 }
